42cbe58df41aa0da271d93439d58cf3f7597d317
kenta0558 <mannheim42@yahoo.de>
2021-04-02 22:07:06 +0200

    add tests for contracts using solidity in Ethereum pet shop

---
+++ Ethereum Pet Shop from trufflesuite.com/test/TestAdoption.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.9.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
+
+    uint expectedPetId = 8;
+
+    address expectedAdopter = address(this);
+
+    function testUserCanAdoptPet() public {
+        uint returnedId = adoption.adopt(expectedPetId);
+
+        Assert.equal(returnedId, expectedPetId, "Adoption of the expected pet should match wwhat is returned.");
+    }    
+
+    function testGetAdopterAddressByPetId() public {
+        address adopter = adoption.adopters(expectedPetId);
+
+        Assert.equal(adopter, expectedAdopter, "Owner of the expected pet should be this contract");
+    }
+
+    function testGetAdopterFromArray() public {
+        address[16] memory adopters = adoption.getAdopters();
+
+        Assert.equal(adopters[expectedPetId], expectedAdopter, "Owner of the expected pet should be this contract");
+    }
+}
