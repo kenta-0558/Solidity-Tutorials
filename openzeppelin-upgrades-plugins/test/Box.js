c324cbe791c7255d4ea1f1414044cf73924668d5
kenta0558 <mannheim42@yahoo.de>
2021-05-30 23:44:34 +0200

    add new file Box.js into test to test Box contract

---
+++ openzeppelin-upgrades-plugins/test/Box.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+let Box;
+let box;
+
+deacribe('Box', () => {
+    beforeEach(async () => {
+        Box = await ethers.getContractFactory('Box');
+        box = await Box.deploy();
+        await box.deployed();
+    });
+
+    it('should return a stored value', async () => {
+        await box.store(42);
+    });
+
+    expect((await box.retrieve()).toString()).to.equal('42');
+});
